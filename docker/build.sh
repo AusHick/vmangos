93cc09ac8747cb952e8e3aee0dc532d854a8374d
AusHick <ajh11m@my.fsu.edu>
2024-01-20 20:32:59 -0500

    docker update

--- docker/build.sh
+++ docker/build.sh
@@ -3,4 +3,3 @@ docker build --pull --target realmd -t realmd:base-vmangos -f Dockerfile ..
 docker build --pull --target mangosd -t mangosd:base-vmangos -f Dockerfile ..
 docker build --pull --target realmd-ptero -t realmd:pterodactyl-vmangos -f Dockerfile ..
 docker build --pull --target mangosd-ptero -t mangosd:pterodactyl-vmangos -f Dockerfile ..
-sleep 10
